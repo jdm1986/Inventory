03896db55f88eace1722cff6c42fe812eb4a932d
Josh Mix <joshuadmix@gmail.com>
2018-03-11 19:40:58 -0400

    Started setting entities, layout to resemble "Machine"

---
+++ Inventory/Entities/Machine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Inventory.Entities
+{
+    public class Machine
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int MachineId { get; set; }
+
+        [Required]
+        public int MachineNum { get; set; }
+
+        [Required]
+        public string MachineMake { get; set; }
+
+        [Required]
+        public string MachineModel { get; set; }
+        public int Hours { get; set; }
+        public string Notes { get; set; }
+        public bool Status { get; set; }
+        public int? TypeId { get; set; }
+        public string Photo { get; set; }
+
+
+        public virtual MachineType MachineType { get; set; }
+    }
+}
